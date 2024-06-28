cb7018c7b49e1b7a0532655435d1ad6388ee2b55
Sneha Tiwari <snehatiwari0620@gmail.com>
2024-06-28 22:27:30 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import MainFile from './components/main';
 
 
 function App() {
     return (
-      <Router>
+      <BrowserRouter>
             <Routes>
-            {/* <Route path="/" element={<Main />} />
-             */}
              <Route path='/' element={<MainFile />} />
             </Routes>
-            </Router>
+            </BrowserRouter>
     );
 }
 
